db64d1152070122cb2122ed5f087d04e9c709661
joó.barnabás <jo.barnabs@C102-07>
2020-09-30 11:45:07 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace FaktoriálisBiginteger
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Számok faktoriálisa 20-30 között");
+            for (int szám = 20; szám <= 10000; szám++)
+            {
+                BigInteger faktor = 1;
+                for (int i = 2; i <= szám; i++)
+                {
+                    faktor = faktor * i;
+                }
+                Console.WriteLine($"{szám}!={faktor}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
